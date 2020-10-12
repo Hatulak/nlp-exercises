c2abc49695d202b51a0001be2e0d2b8966de2de5
hatul <przemek4651@o2.pl>
2020-10-12 20:44:48 +0200

    second exercise

---
+++ code/lesson1/exercise2.py
@@ -0,0 +1,14 @@
+import re
+
+def main():
+
+    with open("maraton.txt", "r") as f:
+        text = f.read()
+
+        print(re.findall("2:[0-9]{2}:[0-9]{2}", text))
+        print(re.findall("[0-9]:[0-9]{2}:[0-9]{2}", text))
+
+
+
+if __name__ == "__main__":
+    main()
